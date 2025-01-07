77f438082a4c2e664a28fd0b83a2733623542e59
Sangamrai10 <ghub487@gmail.com>
2025-01-07 16:25:38 +0545

    404 error page for project

---
+++ src/app/not-found.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function notFound() {
+  return (
+    <div className='font-extrabold flex relative uppercase justify-center items-center h-96'>
+      The page you are looking for is not found!!
+    </div>
+  )
+}
