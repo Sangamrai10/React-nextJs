5c56703e956cde20a7a4474e9e35aecdad5eef41
Sangamrai10 <ghub487@gmail.com>
2025-01-29 09:46:09 +0545

    feat: add product loading and error handling components, and implement product fetching logic

---
+++ src/app/products/page.js
@@ -0,0 +1,41 @@
+"use client"
+import React from 'react';
+import { getAllProducts } from '@/api/products';
+import Card from '@/components/products/card';
+import { useState, useEffect } from 'react';
+import Loading from './loading'
+
+export default function posts({ searchParams }) {
+    const [products, setProducts] = useState([])
+    const [error, setError] = useState(null)
+    const [loadings, setLoadings] = useState(true)
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const data = await getAllProducts(searchParams);
+                setProducts(data);
+            } catch (error) {
+                setError(error)
+            } finally {
+                setLoadings(false)
+            }
+        }
+        fetchData()
+    }, [searchParams])
+    if (loadings) return <Loading/>
+    if (error) return <div>Error: {error.message}</div>
+
+    return (
+        <>
+            <div className='flex items-center justify-between px-5'>
+                <h1>Posts</h1>
+            </div>
+            <div className='flex flex-wrap justify-center p-4 m-4'>
+                {products.map(product => (
+                    <Card key={product.id} product={product} />
+                ))}
+            </div>
+        </>
+    )
+}
