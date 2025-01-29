a7ad691e256a2d9ffbd3aeb0597e13db06ec4ff4
Sangamrai10 <ghub487@gmail.com>
2025-01-29 09:46:24 +0545

    refactor: remove unused blog and posts components to streamline codebase

--- src/app/blog/posts/loading.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Loading() {
-  return (
-    <div className='flex relative justify-center item-center font-bold'>
-      Loading
-    </div>
-  )
-}
