5fe6bc1cfb4f0416b93ac40a39361a7f1f174e32
Sangamrai10 <ghub487@gmail.com>
2025-01-29 09:45:46 +0545

    feat: enhance product card with edit and delete functionality, and improve category and brand links

--- src/components/products/card.js
+++ src/components/products/card.js
@@ -1,21 +1,56 @@
+'use client'
+
 import React from 'react'
 import Image from 'next/image'
 import placeholder from '@/assets/placeholder.png'
+import Link from 'next/link'
+import { Products_Page } from '@/constants/routes'
+import { MdModeEditOutline, MdDelete } from "react-icons/md";
+import { useState } from 'react'
 
 export default function Card({ product }) {
-    const imageUrls = product?.imageUrls || [];
-    const imageUrl = imageUrls.length > 0 ? imageUrls[0] : placeholder;
+    const [showDeletePopup, setShowDeletePopup] = useState(false)
+
+    function removeProduct() {
+        setShowDeletePopup(true)
+    }
+
     return (
-        <div key={product.id} className="max-w-sm m-4 rounded overflow-hidden shadow-lg">
-            <Image className="w-96" src={imageUrl} alt={product.name} width={600} height={400}/>
+        <div key={product.id} className="w-80 m-4 rounded overflow-hidden shadow-lg">
+            <Image width={500} height={500} className="h-40 w-auto mx-auto" src={product.imageUrls.length > 0 ? product.imageUrls[0] : placeholder} alt={product.name} />
             <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2">{product.name}</div>
-            <div className="pb-2">
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{product.category}</span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{product.brand}</span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">eidt icon</span>
-            </div>
-                <p className="text-gray-700 text-base overflow-hidden text-ellipsis max-h-10 ">
+                <div className="pb-2">
+
+                    {/* product category  */}
+                    <span className="inline-block bg-gray-200 rounded-full px-3 m-1 text-sm font-semibold text-gray-700 hover:bg-red-400">
+                        <Link href={``}>
+                            {product.category}
+                        </Link>
+                    </span>
+
+                    {/* product brand  */}
+                    <span className="inline-block bg-gray-200 rounded-full px-3 m-1 text-sm font-semibold text-gray-700 hover:bg-red-400">
+                        <Link href={``}>
+                            {product.brand}
+                        </Link>
+                    </span>
+
+                    {/* products edit icon */}
+                    <span className="inline-block rounded-full text-gray-700 hover:text-red-400">
+                        <Link href={`${Products_Page}/edit/${product.id}`}>
+                            <MdModeEditOutline className='text-lg' />
+                        </Link>
+                    </span>
+
+                    {/* products delete icon  */}
+                    <span className="inline-block rounded-full text-gray-700 hover:text-red-400">
+                        <button onClick={removeProduct}>
+                            <MdDelete className='text-lg' />
+                        </button>
+                    </span>
+                </div>
+                <p className="text-gray-700 text-base truncate max-h-10 ">
                     {product.description}
                 </p>
             </div>
