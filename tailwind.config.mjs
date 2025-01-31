a3f2488fe5ce158e560981dea49de94c129b3153
Sangamrai10 <ghub487@gmail.com>
2025-01-31 20:11:05 +0545

    feat: update Tailwind CSS configuration to include custom primary color palette

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,31 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+
+  const tailwindCongig = {
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          background: "var(--background)",
+          foreground: "var(--foreground)",
+          primary: {
+            50: "#eaeff4",
+            100: "#d4e0e9",
+            200: "#7ea1bd",
+            300: "#5381a7",
+            400: "#3e729c",
+            500: "#286291",
+            600: "#204e74",
+            700: "#183b57",
+            800: "#0c1d2b",
+            900: "#08141d",
+          },
+        },
       },
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+  }
+  export default tailwindCongig;
