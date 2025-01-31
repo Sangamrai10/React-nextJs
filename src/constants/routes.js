1ab761fb605c7281bb383e29e6ed3258001cdb04
Sangamrai10 <ghub487@gmail.com>
2025-01-31 20:35:14 +0545

    feat: add product view constants and restore login page route

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -2,4 +2,4 @@ export const Home_Page = "/";
 export const About_Page = "/about-us";
 export const Contact_Page = "/contact"; 
 export const Products_Page ="/products";
-// export const Login_Page ="/login"
+export const Login_Page ="/login"
