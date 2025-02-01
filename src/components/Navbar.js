1697fdbf504729c2119bdeae324a8e77d5f84e6d
Sangamrai10 <ghub487@gmail.com>
2025-02-01 22:51:05 +0545

    feat: improve Navbar layout with enhanced spacing and readability

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,23 +7,22 @@ import React from 'react'
 
 export default function Navbar() {
     const pathname = usePathname()
-   
+
     return (
-            <nav className="md:w-1/2 flex justify-between" role="navigation">
-                {navLink.map((link, index)=>{ 
-                    const isActive = link.route == Home_Page? 
-                    pathname === link.route: 
+        <nav className="md:w-1/2 flex justify-between gap-4 pt-1" role="navigation">
+            {navLink.map((link, index) => {
+                const isActive = link.route == Home_Page ?
+                    pathname === link.route :
                     pathname.startsWith(link.route)
-                    return (
-                        <Link 
-                        href={link.route} 
-                        key={index} 
-                        className={isActive? "text-blue-500": "text-black"}
-                        >
-                            {link.title}
-                        </Link>
-                    );
-                })}
-            </nav>
+                return (
+                    <Link
+                        href={link.route}
+                        key={index}
+                        className={isActive ? "text-blue-500" : "text-black"}>
+                        {link.title}
+                    </Link>
+                );
+            })}
+        </nav>
     )
 }
