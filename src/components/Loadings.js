5708e6d8c514f9f7ec30596c156efb3e11813f80
Sangamrai10 <ghub487@gmail.com>
2025-02-01 22:49:19 +0545

    feat: update loading component styles and add new route constants

--- src/components/Loadings.js
+++ src/components/Loadings.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Loadings() {
   return (
     <div className='flex relative justify-center item-center mb-4'>
-      <div className="bg-gray-200 w-80 h-72 rounded ms-4">
+      <div className="bg-gray-200 w-64 h-72 rounded ms-4">
 
         {/* image loading  */}
         <div className="bg-gray-400 w-full h-40 animate-pulse"></div>
@@ -18,8 +18,8 @@ export default function Loadings() {
         {/* description section  */}
         <div className="px-4 mt-2 ">
           <div className="bg-gray-400 w-1/2 h-4 rounded animate-pulse"></div>
-          <div className="bg-gray-400 w-52 h-4 my-2 rounded"></div>
-          <div className="bg-gray-400 w-60 h-4 my-2 rounded"></div>
+          <div className="bg-gray-400 w-52 h-4 my-2 rounded animate-pulse"></div>
+          <div className="bg-gray-400 w-60 h-4 my-2 rounded animate-pulse"></div>
         </div>
       </div>
     </div>
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -2,4 +2,6 @@ export const Home_Page = "/";
 export const About_Page = "/about-us";
 export const Contact_Page = "/contact"; 
 export const Products_Page ="/products";
-export const Login_Page ="/login"
+export const Login_Page ="/login";
+export const Register_Page="/register";
+export const Forgot_Password_Page="/forgot-password";
