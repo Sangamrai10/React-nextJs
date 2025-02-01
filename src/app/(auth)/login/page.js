b62162b1beb47b93ae27b028f34b115acc0ba220
Sangamrai10 <ghub487@gmail.com>
2025-02-01 22:49:52 +0545

    feat: enhance login page layout and functionality with password input and navigation links

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -5,12 +5,12 @@ import LoginForm from '@/components/auth/LoginForm'
 
 export default function LoginPage() {
   return (
-    <div className='shadow-xl min-h-[80svh] rounded-2xl flex item-center justify-evenly'>
-      <div className='w-1/2  p-20'>
-        {/* <Image src={ecommerce} alt='auth image' height={300} width={500} className='w-full' /> */}
+    <div className='bg-white min-h-[80svh] rounded-2xl flex item-center justify-evenly p-10 md:p-20 m-5 w-full lg:'>
+      <div className='w-1/2 p-20 hidden md:block'>
+        <Image src={ecommerce} alt='auth image' height={300} width={500} className='w-full' />
       </div>
-      <div>
-        <h4 className='text-3xl mb-5 font-bold'>Login</h4>
+      <div className='w-full md:w-1/3'>
+        <h4 className='text-3xl mb-5 font-bold text-textColor'>Login</h4>
         <LoginForm />
       </div>
     </div>
--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -2,24 +2,24 @@
 
 import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { MdOutlineMailOutline } from 'react-icons/md';
-import { toast } from 'react-toastify';
+import { MdOutlineMailOutline, MdPassword } from 'react-icons/md';
+import { toast, ToastContainer } from 'react-toastify';
 import Link from 'next/link'
-import { Products_Page } from '@/constants/routes'
+import { Forgot_Password_Page, Login_Page, Products_Page, Register_Page } from '@/constants/routes'
+import { usePathname } from 'next/navigation';
 
 export default function LoginForm() {
   // destructuring useForm module form react-hook-form
-    const {
+  const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm();
 
   // loading 
-  const { loading, setLoading } = useState(false)
+  // const { loading, setLoading } = useState(false)
 
   function submitForm(data) {
-    setLoading(true)
     console.log(data)
   }
   // toast container for alert success or errors 
@@ -27,35 +27,53 @@ export default function LoginForm() {
     toast.error(errors, { autoClose: 1500 })
   }, [errors]);
 
+  // check if path is '/login', if true then display forgot password and signup
+  const pathname = usePathname()
+  const isLogin = pathname == Login_Page
+
   return (
-    <form onSubmit={handleSubmit(submitForm)} >
+    <form>
       <div className='py-2'>
+        {/* enter email address */}
         <div className="flex justify-center border-b border-gray-500 items-end my-3">
           <MdOutlineMailOutline className='mb-2' />
           <input
             type='text'
             id='email'
+            name='email'
             placeholder='email'
             className='focus:outline-none bg-transparent w-full px-3 py-1'
             {...register("email", { required: "Email address is required!!" })}
           />
         </div>
         <p className='text-red-600'>{errors.email?.message}</p>
+
+        {/*INPUT PASSWORD*/}
         <div className='flex items-end border-b border-gray-500 my-3'>
+          <MdPassword className='mb-2' />
           <input
             type='password'
-            id='pass'
+            id='password'
+            name='password'
             placeholder='Enter your password!'
             className='focus:outline-none bg-transparent w-full px-3 py-1 '
             {...register("password", { required: "Incorrect password!" })}
           />
         </div>
-        <div>
-          <Link href={`${Products_Page}`} className='text-decoration'>Forgot password?</Link>
-          <Link href={`${Products_Page}`} className='text-decoration'>sign up</Link>
-        </div>
-        <input type="submit" value={loading ? 'submiting....' : 'submit'} className='bg-primary-500 px-4 w-1/4 rounded' />
+        <p className='text-red-600'>{errors.password?.message}</p>
+
+
+        <div className={isLogin ? "block" : "hidden"}>
+          <input type='checkbox' id='rememberMe'/>
+          <label htmlFor='rememberMe'>Remember me</label> <br/>
+          <Link href={`${Forgot_Password_Page}`} className='underline text-primary-400 text-md'>Forgot password?</Link>
+          <Link href={`${Register_Page}`} className='underline text-primary-400 text-md'>sign up</Link>
+        </div><br/>
+        <button type='submit' className='bg-primary-500 px-4 rounded hover:bg-primary-400 text-white'>
+          submit
+        </button>
       </div>
+      <ToastContainer />
     </form>
   )
 }
