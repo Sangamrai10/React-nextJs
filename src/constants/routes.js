d56387007ecfd74c3064e7d53b0431106af5a297
Sangamrai10 <ghub487@gmail.com>
2025-02-02 09:35:19 +0545

    feat: restore Home_Page route and ensure Forgot_Password_Page is defined

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,7 +1,8 @@
-export const Home_Page = "/";
+
 export const About_Page = "/about-us";
 export const Contact_Page = "/contact"; 
 export const Products_Page ="/products";
 export const Login_Page ="/login";
 export const Register_Page="/register";
 export const Forgot_Password_Page="/forgot-password";
+export const Home_Page ="/";
