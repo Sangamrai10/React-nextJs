ec73f07c4480518a5aab8217e6bd281356a5f6c6
Sangamrai10 <ghub487@gmail.com>
2025-02-02 09:35:49 +0545

    feat: update LoginPage styling for improved layout and dark mode support

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -5,12 +5,12 @@ import LoginForm from '@/components/auth/LoginForm'
 
 export default function LoginPage() {
   return (
-    <div className='bg-white min-h-[80svh] rounded-2xl flex item-center justify-evenly p-10 md:p-20 m-5 w-full lg:'>
-      <div className='w-1/2 p-20 hidden md:block'>
+    <div className='bg-white dark:bg-slate-500 min-h-[80svh] rounded-2xl flex item-center justify-evenly p-10 pt-0 md:p-20 m-5 w-full lg:'>
+      <div className='w-1/2 p-10 hidden md:block'>
         <Image src={ecommerce} alt='auth image' height={300} width={500} className='w-full' />
       </div>
-      <div className='w-full md:w-1/3'>
-        <h4 className='text-3xl mb-5 font-bold text-textColor'>Login</h4>
+      <div className='w-full md:w-2/3 lg:w-1/3 dark:text-white'>
+        <h4 className='text-3xl font-bold text-textColor dark:text-white'>Login</h4>
         <LoginForm />
       </div>
     </div>
