2eacb79a37d0040c8dce221a5ca51cb578281ef9
Sangamrai10 <ghub487@gmail.com>
2025-02-05 15:40:45 +0545

    feat: add registerUser function for user registration in auth API

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,5 +1,6 @@
 import config from "@/config/config";
 import axios from "axios";
+
 async function login({email, password}) {
 
     const response = await axios.post(`${config.apiUrl}/api/auth/login`,{
@@ -9,4 +10,15 @@ async function login({email, password}) {
 
     return response.data;
 }
-export {login};
+
+async function registerUser({name, email, password, confirmPassword}){
+    const response = await axios.post(`${config.apiUrl}/api/auth/register`,{
+        name,
+        email, 
+        password,
+        confirmPassword,
+    });
+
+    return response.data;
+}
+export {login, registerUser};
