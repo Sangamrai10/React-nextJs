066ef408f4103887b788f2bd22780f3d46c14e1b
Sangamrai10 <ghub487@gmail.com>
2025-02-06 16:39:14 +0545

    adjusting padding and margins

--- src/app/products/loading.js
+++ src/app/products/loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Loadings from '@/components/Loadings'
 export default function Loading() {
   return (
-    <div className='flex flex-wrap justify-center mt-10'>
+    <div className='flex flex-wrap justify-center pt-10'>
       <Loadings />
       <Loadings />
       <Loadings />
