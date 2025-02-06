ce8a8dca4aab282b33f1105b63b4727c10d4b402
Sangamrai10 <ghub487@gmail.com>
2025-02-06 16:38:45 +0545

    optimizing color scheme

--- src/app/products/layout.js
+++ src/app/products/layout.js
@@ -4,7 +4,7 @@ export const metadata = {
   
   export default function ProductsLayout({ children }) {
     return (
-      <div className="dark:bg-gray-800">
+      <div className="dark:bg-slate-500 bg-gray-100">
         <div className="max-w-screen-xl mx-auto min-h-svh">{children}</div>
       </div>
     );
