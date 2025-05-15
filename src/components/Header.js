57514eb83131a0673fa39f6533c41fc05fd672d5
Sangam Rai <ghub487@gmail.com>
2025-05-15 17:43:22 +0545

    feat: add Header component for navigation and branding

---
+++ src/components/Header.js
@@ -0,0 +1,57 @@
+import { navLink } from "@/constants/navLink";
+import Link from "next/link";
+import React from "react";
+import hamroPasal from "@/assets/hamroPasal.png";
+import { Home_Page, Login_Page } from "@/constants/routes";
+
+function Header() {
+  return (
+    <div className="bg-primary-50 dark:bg-gray-800 dark:text-white">
+      <div className="w-full">
+        <div className="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8"
+        >
+            {/* logo */}
+          <div className="flex flex-row items-center justify-between p-4">
+            <Link href={`${Home_Page}`} className="ps-4 md:ps-0">
+              <img
+                src={hamroPasal.src}
+                alt="logo"
+                className="w-16 h-16 rounded-full"
+              />
+            </Link>
+          </div>
+
+          {/* navigation  */}
+          <nav className="px-2 flex-col flex-grow hidden pb-4 md:pb-0 md:flex md:justify-end md:flex-row">
+            {navLink.map((navlink) => {
+
+                // check if user is authentic 
+              if (navlink.isAuth && !user)
+                return <div key={navlink.route}></div>;
+
+              return (
+                <div key={navlink.route} className="relative px-2">
+                  <Link
+                    key={navlink.title}
+                    className="py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                    href={navlink.route}
+                  >
+                    {navlink.title}
+                  </Link>
+                </div>
+              );
+            })}
+          </nav>
+          <Link
+            href={`${Login_Page}`}
+            className="p-1 px-2 rounded text-white text-sm bg-primary-500"
+          >
+            LOGIN
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
