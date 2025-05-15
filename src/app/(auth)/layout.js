68cedf6b2195270bceb314c064f16fc1cdfcc07a
Sangam Rai <ghub487@gmail.com>
2025-05-15 17:43:00 +0545

    refactor: clean up layout component and improve user authentication handling

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -1,14 +1,29 @@
-'use client'
+"use client";
 
-import React from 'react'
+import React, { useEffect } from "react";
+import getAuthUser from '@/helpers/authUser';
+import { useRouter } from 'next/navigation';
+import { Home_Page } from "@/constants/routes";
 
 export default function layout({ children }) {
 
+
+  const authUser = getAuthUser()
+
+  const router = useRouter()
+
+    console.log(authUser)
+
+  // if user is already logged in, redirect to home page
+  useEffect(() => {
+    if (authUser) {
+      router.push(Home_Page);
+    }
+  }, [authUser, router]);
+  
   return (
-    <div className='bg-gray-100 dark:bg-slate-500'>
-      <div className='max-w-screen-xl py-4 ps-4 min-h-svh'>
-        {children}
-      </div>
+    <div className="bg-gray-100 dark:bg-slate-500">
+      <div className="max-w-screen-xl py-4 ps-4 min-h-svh">{children}</div>
     </div>
-  )
+  );
 }
