7e9eca63de73b72760e6a527e5fc5303e5ab17b7
Sangam Rai <ghub487@gmail.com>
2025-05-15 17:42:52 +0545

    feat: implement product fetching and display in Home component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,35 @@
+"use client";
+import React from "react";
 import Loading from "./products/loading";
+import { useState} from "react";
+import { getAllProducts } from "@/api/products";
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    setLoading(true);
+    try {
+      const response = await getAllProducts();
+      setProducts(response.data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    } finally {
+      setLoading(false);
+    }
+  }
   return (
     <>
       <div className="flex justify-center content-center">this is  Home page </div>
       <div className='flex flex-wrap justify-center'>
-        <Loading/>
+        {products.map((product) => (
+          <div key={product.id} className="m-4 p-4 border rounded">
+            <h2 className="text-lg font-bold">{product.name}</h2>
+            <p>{product.description}</p>
+            <p>Price: ${product.price}</p>
+            <p>Stock: {product.stock}</p>
+          </div>
+        ))}
       </div>
 
     </>
