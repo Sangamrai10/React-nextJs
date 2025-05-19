b3193f0f1597e3b3030762608a75ccd7a21df06c
Sangam Rai <ghub487@gmail.com>
2025-05-19 18:53:23 +0545

    feat: enhance product details display with structured layout and image component

--- src/app/products/[productId]/page.js
+++ src/app/products/[productId]/page.js
@@ -1,19 +1,45 @@
-import React from 'react'
-import { getProductById } from '@/api/products'
+import React from "react";
+import { getProductById } from "@/api/products";
+import Image from "next/image";
+import kaneki from "@/assets/kaneki.jpg";
+import { MdOutlineStarPurple500, MdStarHalf } from "react-icons/md";
 
-export default async function ProductByIdPage({params}) {
-    const id = (await params).productId
-    const product = await getProductById(id)
-    console.log(product)
+export default async function ProductByIdPage({ params }) {
+  const id = (await params).productId;
+  const product = await getProductById(id);
+  console.log(product);
   return (
-  <div>
-    <div>product id: {id}</div>
-    <div>product name: {product.name}</div>
-    <div>product price: {product.price}</div>
-    <div>product description: {product.description}</div>
-    <div>product category: {product.category}</div>
-    <div>product brand: {product.brand}</div>
-    <div>product created at: {product.createdAt}</div>
-</div>
-  )
+    <div className="bg-slate-100 w-full">
+      <div className="container mx-auto min-h-[90svh] flex justify-evenly items-center">
+        <div>
+          <Image
+            src={product.imageUrls.length > 0 ? product.imageUrls[0] : placeholder}
+            alt={product.name}
+            width={200}
+            height={300}
+            className="rounded-lg min-h-[70svh] w-auto p-2 drop-shadow"
+          />
+        </div>
+        <div className="bg-white min-h-[90svh] w-1/2 p-20">
+          <p className="font-bold uppercase text-gray-600">
+            {product.category}
+          </p>
+          <h1 className="font-semibold text-3xl">{product.name}</h1>
+          <div className="flex gap-2">
+            <div className="flex gap-2 mt-1">
+              <MdOutlineStarPurple500 className="text-yellow-500" />
+              <MdOutlineStarPurple500 className="text-yellow-500" />
+              <MdOutlineStarPurple500 className="text-yellow-500" />
+              <MdOutlineStarPurple500 className="text-yellow-500" />
+              <MdStarHalf className="text-yellow-500" />
+            </div>
+            <span className="">4.4(400 review)</span>
+          </div>
+          <h4 className="p-1 px-2 bg-primary-200 rounded w-fit">
+            {product.brand}
+          </h4>
+        </div>
+      </div>
+    </div>
+  );
 }
